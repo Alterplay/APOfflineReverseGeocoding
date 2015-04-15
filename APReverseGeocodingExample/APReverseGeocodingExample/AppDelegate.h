d72d44c0262b8ce353784a15a1f9a65cc6f186c9
Serg Krivoblotsky <krivoblotsky@me.com>
2015-04-15 12:18:21 +0300

    Expample project initiated

---
+++ APReverseGeocodingExample/APReverseGeocodingExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  APReverseGeocodingExample
+//
+//  Created by Sergii Kryvoblotskyi on 4/15/15.
+//  Copyright (c) 2015 Sergii Kryvoblotskyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ APReverseGeocodingExample/APReverseGeocodingExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  APReverseGeocodingExample
+//
+//  Created by Sergii Kryvoblotskyi on 4/15/15.
+//  Copyright (c) 2015 Sergii Kryvoblotskyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
